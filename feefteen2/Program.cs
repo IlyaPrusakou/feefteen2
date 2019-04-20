1bab7e51ef6391cf6659125b71200d371ce3c5bb
Ilya Prusakou <beyourself@tut.by>
2019-04-20 22:22:05 +0400

    dfsdf

--- feefteen2/Program.cs
+++ feefteen2/Program.cs
@@ -10,6 +10,22 @@ namespace feefteen2
     {
         static void Main(string[] args)
         {
+            string s = typeof(Program).Assembly.FullName;
+            string g = typeof(game).Assembly.FullName;
+            Console.WriteLine("program   " + s);
+            Console.WriteLine("game   " + g);
+            game Game = new game(4);
+            Game.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine($"{game.mas[i, j]} --- {Game.GetNumber(game.mas[i, j])}");
+                }
+            }
+            int Test = Game.GetNumber(5);
+            Console.WriteLine(Test);
+            Console.ReadLine();
         }
     }
 }
---
+++ feefteen2/game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace feefteen2
+{
+    class game
+    {
+        public int size;
+        public int zeroX;
+        public int zeroY;
+        public int position;
+        public int[,] mas;
+
+
+
+        public game(int Size)
+        {
+            if (Size < 2) { Size = 2; }
+            if (Size > 5) { Size = 5; }
+            size = Size;
+            mas = new int[size, size];
+        }
+
+        public void Start()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    mas[x, y] = CoordToPosition(x, y) + 1;
+                }
+            }
+            zeroX = size - 1;
+            zeroY = size - 1;
+            mas[zeroX, zeroY] = 0;
+        }
+
+        public int GetNumber(int position)
+        {
+            int x, y;
+            PositionToCoord(position, out x, out y);
+            if (x < 0 || x > size) { return 0; }
+            if (y < 0 || y > size) { return 0; }
+            return mas[x, y];
+        }
+
+        private int CoordToPosition(int x, int y)
+        {
+            return position = y * size + x;
+        }
+
+        private void PositionToCoord(int position, out int x, out int y)
+        {
+            x = position % size;
+            y = position / size;
+        }
+
+
+    }
+}
